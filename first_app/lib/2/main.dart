98bd98605d8b51500ac40b1730c2a0620960fefb
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2025-04-25 23:22:52 +0300

    Color object created

--- first_app/lib/2/main.dart
+++ first_app/lib/2/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Scaffold Yapısı'))),
+      home: Scaffold(
+        //backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 47, 5, 120),
+        body: Center(child: Text('Background Color Added')),
+      ),
     ),
   );
 }
